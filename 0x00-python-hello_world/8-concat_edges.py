75e55d62acc3313ea6a52e1bfab78cc81f6eb74f
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-10 08:09:48 +0000

    check 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split(", ")
+str = str[2][:27] + ' ' + str[2][-22:-18] + ' ' + str[0][:6]
+print(str)
