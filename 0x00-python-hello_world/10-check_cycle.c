5076fd869bfa7bc8b308b86d75b5f7d7bcd02328
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-10 09:01:57 +0000

    check for cycles

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * check_cycle - function checks if a linked list has a cycle
+  * @list: head of the list
+  *
+  * Return: 0 if no cycle, 1 if cycle found
+  */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = NULL;
+
+	if (list == NULL)
+	{
+		perror("Head of the linked list is NULL\n");
+		return (1);
+	}
+	current = list;
+	while (current->next != NULL)
+	{
+		if (current->next == list)
+		{
+			return (1);
+		}
+		current = current->next;
+	}
+	return (0);
+}
