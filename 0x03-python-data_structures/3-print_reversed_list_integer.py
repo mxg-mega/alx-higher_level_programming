cad173cc919101be67fb82110e8be4a676dfb5a2
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-21 20:19:32 +0000

    print the list in reverse foxed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(1, (len(my_list) + 1)):
-        index = i * -1
+    for i in range(len(my_list)):
+        if len(my_list) == 0:
+            break;
+        index = (i + 1) * -1
         str = '{:d}'
         print(str.format(my_list[index]))
