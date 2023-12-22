7baefdfe609706655984c97843fb13878f702213
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-22 08:14:08 +0000

    matrices in python

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            str = '{}'
+            if j == i[-1]:
+                print(str.format(j), end='')
+            else:
+                print(str.format(j), end=' ')
+        print()
