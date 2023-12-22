cc949b1bbd89e941ad14b3059e3218a535b56cd9
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-22 04:02:15 +0000

    a None initailaized list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return None
     for i in range(len(my_list)):
         if len(my_list) == 0:
-            break;
+            break
         index = (i + 1) * -1
         str = '{:d}'
         print(str.format(my_list[index]))
