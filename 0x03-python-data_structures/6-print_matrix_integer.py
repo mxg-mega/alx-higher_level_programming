d9d8d3525675b316d8f2e630b2cf18e3d9dbd055
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-22 08:30:50 +0000

    matrices in python fix 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            str = '{}'
+            str = '{:d}'
             if j == i[-1]:
                 print(str.format(j), end='')
             else:
