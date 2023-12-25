cabb92bfb6bb864f9b1a0f3626a2a0883d70db57
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-25 20:09:47 +0000

    square of all matrices

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if len(matrix) is None:
+        return None
+
+    square = []
+    for i in range(len(matrix)):
+        if len(matrix[i]) == 0 or matrix[i] is None:
+            continue
+        squares = [matrix[i][s]**2 for s in range(len(matrix[i]))]
+        square.append(squares)
+    return square
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+In this repo we treat more data structures in python like tuples and sets, so the solution for the 0x04-python-more_data_structures are treated here
