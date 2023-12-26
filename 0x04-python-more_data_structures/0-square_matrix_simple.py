f295864c6630a57b43b42faf4e80423b9f78e07d
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-26 04:10:49 +0000

    square of all matrices fix2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,10 @@ def square_matrix_simple(matrix=[]):
     if len(matrix) is None:
         return None
 
-    square = [[]]
+    square = []
     for i in range(len(matrix)):
         if len(matrix[i]) == 0 or matrix[i] is None:
+            square.append([])
             continue
         squares = [matrix[i][s]**2 for s in range(len(matrix[i]))]
         square.append(squares)
