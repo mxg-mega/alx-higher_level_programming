6bb6c08b473f39ec8b6d126b4d4da7ffd2b21e22
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-28 14:55:58 +0000

    changed the code for matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) is None:
-        return None
-
-    square = []
-    for i in range(len(matrix)):
-        if len(matrix[i]) == 0 or matrix[i] is None:
-            square.append([])
-            continue
-        squares = [matrix[i][s]**2 for s in range(len(matrix[i]))]
-        square.append(squares)
-    return square
+    def square(a_list=[]):
+        return list(map((lambda x: x * x), a_list))
+    return list(map(square, matrix))
