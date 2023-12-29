8b7db66e4722edee0093ef5fd05724be76bb837b
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-12-29 09:41:17 +0000

    sort a dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None:
-            print()
+        print()
     sorted = [i for i in a_dictionary.keys()]
     sorted.sort()
     for i in sorted:
