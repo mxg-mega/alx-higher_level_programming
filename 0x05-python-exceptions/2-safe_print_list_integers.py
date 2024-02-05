f8576ddf15ece558ebf0b3e9638cb586da20b416
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-05 22:17:39 +0000

    safe print integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except ValueError:
+            count += 1
+            continue
+        except (IndexError, TypeError):
+            break
+    print()
+    return (i - count)
