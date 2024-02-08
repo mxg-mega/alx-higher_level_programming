aaacadf4bd2d4a3dca04357453dac8b67b8e07cc
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-08 14:54:53 +0000

    check for docstring 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 class Square():
-    """ A class named Square
-    Attributes:
-        __size(int): the size of the square as a class
+	"""Exceptions are documented in the same way as classes.
 
-    methods:
-        __init__: the initialization of the sqaure class
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+
+    Either form is acceptable, but the two should not be mixed. Choose one
+    convention to document the __init__ method and be consistent with it.
+
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        size (int): size of the square
+
+    Attributes:
+        __size (int): size of the square
+
     """
     def __init__(self, size):
-        """ __init__ is a method that initializes the instances of the class
-        Args:
-            size(int): integer to know the size of the square
-        """
         self.__size = size
