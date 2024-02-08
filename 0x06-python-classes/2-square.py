8b43e84791c6b8dbbe99344e0fae5bb076388c0a
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-08 15:54:05 +0000

    argument size can only be an int fix 1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,20 +6,13 @@ class Square():
     """ DocString of the Square class
     Attributes:
         __size (int): size of the square
+    Args:
+        size (int): size of the sqaure
     Raises:
         ValueError: If the size value is less than 0.
         TypeError: If the size attribute is not an int.
     """
     def __init__(self, size=0):
-		"""Initializes the Square instance with a given size.
-
-        Args:
-            size (int): Size of the square.
-
-        Raises:
-            ValueError: If the size value is less than 0.
-            TypeError: If the size attribute is not an int.
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
