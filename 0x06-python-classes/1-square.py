529ad2c01943cf11870e630edfd50a29e94adff4
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-08 14:50:22 +0000

    check for docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 class Square():
+    """ A class named Square
+    Attributes:
+        __size(int): the size of the square as a class
+
+    methods:
+        __init__: the initialization of the sqaure class
+    """
     def __init__(self, size):
+        """ __init__ is a method that initializes the instances of the class
+        Args:
+            size(int): integer to know the size of the square
+        """
         self.__size = size
