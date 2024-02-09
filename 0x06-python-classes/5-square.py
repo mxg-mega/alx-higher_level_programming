85eae0ff026fcb38386b8bb962c19c2e5409111e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-09 13:49:24 +0000

    print a # square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Square Class
+    Attributes:
+        __size (int): the size of the square
+"""
+
+
+class Square():
+    """__init__ initalizes the class
+    Args:
+        size: the size of square
+    Raises:
+        ValueError: if value is less than 0
+        TypeError: if not an integer
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise
+        if size < 0:
+            raise
+        self.__size = size
+
+    """Area - calculates the area of a square
+    Args:
+        non
+    Returns:
+        int: the area of a square
+    """
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter Method
+        Returns:
+            int: the value of __size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """Setter Method
+        Args:
+            size (int): the new size of square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    """my_print - function prints #
+    in a sqaure shape of size X size
+    """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
