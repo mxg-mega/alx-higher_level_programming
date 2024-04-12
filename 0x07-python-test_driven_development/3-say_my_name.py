204b22588f83af3f0de9ff338002aeb0ad70d899
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-12 10:48:18 +0000

    final fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,5 +20,7 @@ def say_my_name(first_name="", last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str) or last_name is None:
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    if first_name == "" and last_name == "":
+        print()
+    else:
+        print("My name is {:s} {:s}".format(first_name, last_name))
