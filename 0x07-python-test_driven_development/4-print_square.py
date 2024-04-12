4898f4ac70bc04d9acfa21b66dea347ee00e3242
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-12 15:31:56 +0000

    print square tests done

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Funtions to print a square
+"""
+
+
+def print_square(size):
+    if not isinstance(size, (int, float)) or (type(size) is float and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    try:
+        s = int(size)
+    except (ValueError, TypeError):
+        raise TypeError("size must be an integer")
+
+    for i in range(s):
+        for j in range(s):
+            print("#", end='')
+        print()
+    
