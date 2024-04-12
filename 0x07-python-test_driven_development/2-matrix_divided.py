f490c2a26cca544ade1834e588f20ffbdf6b722e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-12 06:01:06 +0000

    checks done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,25 +6,25 @@
 def matrix_divided(matrix, div):
     """ function divides the elements
         of matriox by div
-	Args:
-	    matrix(list): multidimentional list
+        Args:
+            matrix(list): multidimentional list
             div(int): number to divide each element
 
-	Return:
-	    list: a multidimentional array
+        Return:
+            list: a multidimentional array
 
-	Exceptions:
-	    TypeError: if not equal element on each array
-	    ZeroDivisionError: div is 0
-    """
+        Exceptions:
+            TypeError: if not equal element on each array
+            ZeroDivisionError: div is 0
+    """ 
     new_matrix = []
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if isinstance(matrix, list):
         if not all(isinstance(item, list) for item in matrix):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raiseMatrixTypeError()
     elif matrix is None or not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raiseMatrixTypeError()
     if div == 0:
         raise ZeroDivisionError("division by zero")
     try:
@@ -38,8 +38,15 @@ def matrix_divided(matrix, div):
         if init_len != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
         if not all(isinstance(item, (int, float)) for item in matrix[i]):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raiseMatrixTypeError()
         for j in range(init_len):
             row.append(float("{:.2}".format(matrix[i][j] / div)))
         new_matrix.append(row)
     return new_matrix
+
+def raiseMatrixTypeError():
+    """The function raises a type Error for
+       Errors with matrix collected in matrx_divided
+    """
+    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
