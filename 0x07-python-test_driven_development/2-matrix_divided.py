0e7b269fb7097c57f7c2e940030a8dce1b3e8283
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-12 05:48:16 +0000

    done with task 2 tests

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" function that divides the matrix provided
+"""
+
+
+def matrix_divided(matrix, div):
+    """ function divides the elements
+        of matriox by div
+	Args:
+	    matrix(list): multidimentional list
+            div(int): number to divide each element
+
+	Return:
+	    list: a multidimentional array
+
+	Exceptions:
+	    TypeError: if not equal element on each array
+	    ZeroDivisionError: div is 0
+    """
+    new_matrix = []
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if isinstance(matrix, list):
+        if not all(isinstance(item, list) for item in matrix):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    elif matrix is None or not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    try:
+        d = int(div)
+    except (ValueError, TypeError):
+        raise TypeError("div must be a number")
+
+    init_len = len(matrix[0])
+    for i in range(len(matrix)):
+        row = []
+        if init_len != len(matrix[i]):
+            raise TypeError("Each row of the matrix must have the same size")
+        if not all(isinstance(item, (int, float)) for item in matrix[i]):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for j in range(init_len):
+            row.append(float("{:.2}".format(matrix[i][j] / div)))
+        new_matrix.append(row)
+    return new_matrix
