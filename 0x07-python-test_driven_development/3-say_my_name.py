c86b44c7ff9591f36d638e853ce2f59150b02720
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-12 10:06:53 +0000

    how to test string printing

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" The functions here says the name inputed
+    every function will be tested in the tests directory
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ function prints out
+    'My name <first_name> <last_name>>'
+
+    Args:
+        first_name(string): first string
+        last_name(string): second string
+    Return:
+        no Return
+    Exceptions:
+        TypeError: all names must be string
+    """
+    if not isinstance(first_name, str) or first_name is None:
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str) or last_name is None:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
