58ef35d9ae4cdce67cf47348ee4c8457963a15cd
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-14 16:49:32 +0000

    highest integer at beginning or end

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-11, 3, 5, 8]), 8)
         self.assertEqual(max_integer([-1, 0]), 0)
         self.assertEqual(max_integer([1.5, 0.4, 5.2]), 5.2)
+        self.assertEqual(max_integer([1, 2, 3, 1.5, 2.5]), 3)
+        self.assertEqual(max_integer([500, 200, 45, 100]), 500)
         self.assertIsNone(max_integer([]))
         self.assertIsNone(max_integer())
 
