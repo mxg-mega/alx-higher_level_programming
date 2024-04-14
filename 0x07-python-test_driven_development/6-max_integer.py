3d5f32a242fb50c461bb18e3b42ebfaed56e1ce9
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-14 16:43:28 +0000

    checks on negative number fix 2

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -7,13 +7,22 @@ def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
-
     if len(list) == 0:
         return None
-    result = list[0]
+
+    # If one number is negative, exchange it to a positive number
+    copy_list = list[:]
+    i = 0
+    while i < len(copy_list):
+        if copy_list[i] < 0:
+            copy_list[i] *= -1
+            break
+        i += 1
+
+    result = copy_list[0]
     i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
+    while i < len(copy_list):
+        if copy_list[i] > result:
+            result = copy_list[i]
         i += 1
     return result
