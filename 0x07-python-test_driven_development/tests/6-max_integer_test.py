22ae84d7649857adecbd1934c2d5122f213fd259
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-14 16:36:00 +0000

    checks using unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,13 +13,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1]), 1)
         self.assertIsNone(max_integer())
 
-    def test_max_integer_raises(self):
-        """"""
-        self.assertRaises(TypeError, max_integer, 1)
-        self.assertRaises(TypeError, max_integer, float("nan"))
-        self.assertRaises(TypeError, max_integer, "list")
-        self.assertRaises(TypeError, max_integer, True)
-        self.assertRaises(TypeError, max_integer, [1, "2", 3])
 
 if __name__ == '__main__':
     unittest.main()
