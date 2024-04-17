c984ea544d2009586e8945e15991f3397828ea99
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-17 08:33:19 +0000

    fixed the exception errors

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,14 @@
 class Rectangle:
     """ initialization function for class rectangle """
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
@@ -22,7 +30,7 @@ class Rectangle:
                 TypeError: value has to be an integer
                 ValueError: width must be >= 0
         """
-        if not isinstance(value, (int, float)):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -42,7 +50,7 @@ class Rectangle:
                TypeError: value has to be an integer
                ValueError: height must be >= 0
        """
-        if not isinstance(value, (int, float)):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
