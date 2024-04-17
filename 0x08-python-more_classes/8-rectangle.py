b6cc1e03ac7267d76b4d0b46ffcbdc43f671bed8
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-17 18:23:13 +0000

    static methods for which rectangle is bigger

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,13 +75,13 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-       if not isinstance(rect_1, Rectangle):
-           raise TypeError("rect_1 must be an instance of Rectangle")
-       if not isinstance(rect_2, Rectangle):
-           raise TypeError("rect_2 must be an instance of Rectangle")
-       if rect_1.area() > rect_2.area() or rect_1.area() == rect_2.area():
-           return rect_1
-       return rect_2
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area() or rect_1.area() == rect_2.area():
+            return rect_1
+        return rect_2
 
     def __str__(self):
         """ prints rep of rectangle using print_symbol """
