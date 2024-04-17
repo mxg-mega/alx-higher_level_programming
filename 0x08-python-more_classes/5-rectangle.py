5abf63ad6f949f0fa2f594ed3fa25a357a1b6a35
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-17 12:18:52 +0000

    deleting instances will alert the user

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -82,5 +82,6 @@ class Rectangle:
     def __repr__(self):
         """ representation of class """
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
     def __del__(self):
         print("Bye rectangle...")
