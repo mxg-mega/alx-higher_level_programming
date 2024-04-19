3260b783a283fba2286b4e63d13818b08499f546
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-19 11:32:05 +0000

    lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function should lookup on objects """
+
+
+def lookup(obj):
+    """that returns the list of available
+        attributes and methods of an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Inheritance is a fun OOP concet allowing modularization and time efficient reuse of codes
