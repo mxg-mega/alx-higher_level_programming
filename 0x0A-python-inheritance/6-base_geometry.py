5f8c51a438d96897f65b7951858ec296cd8fb604
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-19 18:51:53 +0000

    non implemented function area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" BaseGeometry Class """
+
+
+class BaseGeometry:
+    """ Base Geometery class """
+
+    """ function area is a non implemented function """
+    def area(self):
+        raise Exception("area() is not implemented")
