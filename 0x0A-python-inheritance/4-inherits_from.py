d4e602267cc898c974bb725b8570e42a651f8898
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-19 18:21:07 +0000

    direct or indirect inheritance fix 2

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,4 +16,6 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a class inherited
               from a_class; otherwise False.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
