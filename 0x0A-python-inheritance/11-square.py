cd2641af41b17d12158039d033667be2888bb6a6
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-20 13:24:34 +0000

    custom str for square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,29 @@
-nk
+#!/usr/bin/python3
+""" Square class """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ This class is a subclass of Rectangle """
+    """ __init__ instanciates the class with size
+
+        Args:
+            size(int): size value of the square
+
+        raises:
+            TypeError: this is from the superclass validator
+                function.
+            ValueError: this is from the validator function
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    """area return the area of a Square"""
+    def area(self):
+        return self.__size ** 2
+
+    """Returns a string representation of the square"""
+    def __str__(self):
+        return "[Square] {}/{}".format(size, size)
