e6ac886c0d3c70905f0632955f9c60da5ab0861a
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-21 16:47:48 +0000

    serialization for custom class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class_to_json Function"""
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary description
+        with simple data structure
+        (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object
+
+        Args:
+            obj: instance of a class
+
+        Return:
+            a dictionary representation
+    """
+    attributeDict =  obj.__dict__
+    return attributeDict
