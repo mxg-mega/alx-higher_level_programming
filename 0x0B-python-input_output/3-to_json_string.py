2a23ee4e75d719ed944f276b53e9536886af88d7
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-21 10:10:21 +0000

    serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" to_json_strin funtion """
+json = __import__('json')
+
+def to_json_string(obj):
+    """ function returns the JSON representation of an object (string)
+
+        Args:
+            obj: the object to serialize
+        Return:
+            rep: representation of the object
+    """
+    rep = json.dumps(obj)
+    return rep
