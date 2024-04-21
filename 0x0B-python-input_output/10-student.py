59255722f4d712d8c3b90d1e5aaa4c408dfb849c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-21 17:34:07 +0000

    attribute lookup

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Student Class """
+
+
+class Student:
+    """ Student Class
+        Attributes:
+            first_name(public, str): first name of student
+            last_name(public, str): last name of student
+            age(public, int): age of student
+    """
+
+    """ Init istantiates the class
+        Args:
+            first_name(str): first name of student
+            last_name(str): last name of student
+            age(int): age of student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ function retreives a dictionary representation
+            of class instance
+            Args:
+                attrs: list of attributes to get
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            attrsLookup = {}
+            for i in attrs:
+                try:
+                    attrsLookup[i] = self.__dict__[i]
+                except Exception:
+                    continue
+        return attrsLookup
