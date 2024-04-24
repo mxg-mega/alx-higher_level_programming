72ad4acae1a4f8e97fceec4392761b95a9fa56fb
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-24 12:17:00 +0000

    task 1 and 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            self.id = id
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
