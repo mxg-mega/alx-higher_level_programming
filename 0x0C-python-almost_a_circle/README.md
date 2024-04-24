6dae59829a1ce8a79c25af1df292c06712e8ec39
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-24 12:13:44 +0000

    task 1 and 2

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,2 @@
+To prepare for the upcoming AirBnB Clone project using python this project is Done.
+this project helps understand *args and **kwargs, Unit testing using Unittest
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Base Class """
+
+
+class Base:
+    """ Base class
+        Attributes:
+            nb_objects: private class atrributes rep
+            Nyumber of objects created.
+    """
+    __nb_objects = 0
+
+    """ __init__ instantiates the class
+
+        Args:
+            id(int): id of an object,
+            initialized to None.
+    """
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.id = id
+            Base.__nb_objects += 1
