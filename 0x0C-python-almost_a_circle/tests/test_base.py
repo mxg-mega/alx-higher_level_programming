660433ea78673d23ba452964904e88a7d54ef88a
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-26 17:15:42 +0000

    unittest for all base and rectangle

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -32,26 +32,51 @@ class TestRecangle_instantiation(unittest.TestCase):
     def test_auto_id(self):
         """Test Base class id management"""
         r2 = Rectangle()
-        self.assertEqual(r2.id, 4)
+        self.assertEqual(r2.id, 5)
 
         r3 = Rectangle()
-        self.assertEqual(r3.id, 5)
+        self.assertEqual(r3.id, 6)
 
     def test_custom_id(self):
         r1 = Rectangle(0, 0, 0, 0, 12)
         self.assertEqual(r1.id, 12)
 
     def test_None(self):
-        r1 = Rectangle(None)
-        self.assertEqual(r1.id, 7)
+        r1 = Rectangle(0, 0, 0, 0, None)
+        self.assertEqual(r1.id, 4)
 
     def test_attributes(self):
-        r1 = Rectangle(6, 5, -4, 4, 34)
+        r1 = Rectangle(6, 5, 45, 4, 34)
         self.assertEqual(r1.id, 34)
-        self.assertEqual(r1.x, -4)
+        self.assertEqual(r1.x, 45)
         self.assertEqual(r1.y, 4)
         self.assertEqual(r1.width, 6)
         self.assertEqual(r1.height, 5)
+
+    def test_getter_setter(self):
+        r1 = Rectangle(0, 0, 0, 0, 45)
+        r1.width = 23
+        self.assertEqual(r1.width, 23)
+        r1.height = 12
+        self.assertEqual(r1.height, 12)
+        r1.x = 5
+        self.assertEqual(r1.x, 5)
+        r1.y = 2
+        self.assertEqual(r1.y, 2)
+
+    def test_raises(self):
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-1, 0, 0, 0, 0)
+            r2 = Rectangle(0, -1, 0, 0, 0)
+            r3 = Rectangle(0, 0, -1, 0, 0)
+            r4 = Rectangle(0, 0, 0, -1, 0)
+
+        with self.assertRaises(TypeError):
+           r1 = Rectangle("0", 0, 0, 0, 0)
+           r2 = Rectangle(0, "height", 0, 0, 0)
+           r3 = Rectangle(0, 0, "x", 0, 0)
+           r4 = Rectangle(0, 0, 0, "y", 0)
+
 
 
 if __name__ == "__main__":
