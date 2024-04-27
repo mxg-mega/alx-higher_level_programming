376b30ced5dd9f6b5516d4757838c16f16ff2862
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-27 17:06:59 +0000

    to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Base Class """
+import json
 
 
 class Base:
@@ -22,3 +23,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ The static method to_json_string
+            that returns the JSON string representation
+            of list_dictionaries.
+
+            Args:
+                list_dictionaries(list): the data to
+                                         convert to json format
+        """
+        if type(list_dictionaries) is not list:
+            raise TypeError("Data must be a list of dictionaries")
+        if list_dictionaries == None:
+            return "[]"
+        json_string = json.dumps(list_dictionaries)
+        return json_string
