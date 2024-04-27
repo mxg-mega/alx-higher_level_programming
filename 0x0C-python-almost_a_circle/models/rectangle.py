c155705d4c29a9202e0c6076cf1a00a7f931f691
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-04-27 20:08:31 +0000

    proper create

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,9 +133,16 @@ class Rectangle(Base):
         length = len(args)
         if length == 0:
             for name, value in kwargs.items():
-                if not hasattr(self, name):
-                    continue
-                setattr(self, name, value)
+                if name == "id":
+                    self.id = value
+                elif name == "width":
+                    self.__width = value
+                elif name == "height":
+                    self.__height = value
+                elif name == "x":
+                    self.__x = value
+                elif name == "y":
+                    self.__y = value
         else:
             for i in range(length):
                 if i == 0:
