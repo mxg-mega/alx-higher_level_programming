cc5964389e5974a2a2e04870e44fe1c1b4c63aa7
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-08-10 14:57:35 +0000

    show ratings

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Script to list all shows by their rating sum
+
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
