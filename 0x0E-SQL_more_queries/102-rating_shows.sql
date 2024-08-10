f86880b5d5869ad3342964abadf543640331bd49
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-08-10 15:02:45 +0000

    rating shows fix 2

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script to list all shows by their rating sum
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
